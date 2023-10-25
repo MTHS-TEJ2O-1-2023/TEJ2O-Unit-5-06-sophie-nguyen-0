9a63780ef95ab6dee193564f0a5941026b2cdc61
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-25 12:47:20 -0400

    code

--- main.ts
+++ main.ts
@@ -5,4 +5,17 @@
  * This program detects distance with sonar
 */
 
-basic.showString('Hello, World!')
+// variables
+let distance: number = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Rollerskate)
+
+// find distance
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+  basic.showNumber(distance)
+  basic.showIcon(IconNames.Fabulous)
+})
