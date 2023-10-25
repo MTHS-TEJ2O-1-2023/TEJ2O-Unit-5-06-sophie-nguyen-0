11206b2c07a22a8ae7eb00424d0475ba42f8b8ee
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-25 12:43:13 -0400

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sophie
+ * Created on: Oct 2023
+ * This program detects distance with sonar
 */
 
 basic.showString('Hello, World!')
